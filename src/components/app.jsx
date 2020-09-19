5225ed35270af66217d168d62dfbd06f1e899f08
Ernesto Alvarado <alvaradoernesto@hotmail.com>
2020-09-19 18:12:00 -0300

    finish my first react app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,55 @@
-import React , { Component } from 'react';
+import React, { Component } from 'react';
+import giphy from 'giphy-api';
 
-import SearchBar from './search_bar.jsx';
-import Gif from './gif.jsx';
-import GifList from './gif_list.jsx';
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
+const GIPHY_API_KEY = '1KMPHCBIOe3hOjJwCJQX49sRc6cM0oIm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+  }
+
+  search = (query) => {
+    giphy({ apiKey: GIPHY_API_KEY, https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (err, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="xT9IgDEI1iZyb2wqo8" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList gif />
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
         </div>
       </div>
-      );
+    );
   }
 }
 
--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react';
+import Gif from './gif';
 
-class GifList extends Component {
-  render() {
-    <Gif id=1 />, <Gif id=2 />
-  }
-}
-
-export default Gif;
+const GifList = (props) => {
+  return (
+    <div className="gif-list">
+      {props.gifs.map(gif => <Gif id={gif.id} key={gif.id} selectGif={props.selectGif} />)}
+    </div>
+  );
+};
+export default GifList;
