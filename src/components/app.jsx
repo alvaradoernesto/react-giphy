8e9a979c8dfa97a398c64b1b185ed92f65cc6243
Ernesto Alvarado <alvaradoernesto@hotmail.com>
2020-09-19 15:38:17 -0300

    finish half page

---
+++ src/components/app.jsx
@@ -0,0 +1,25 @@
+import React , { Component } from 'react';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id="xT9IgDEI1iZyb2wqo8" />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gif />
+        </div>
+      </div>
+      );
+  }
+}
+
+export default App;
---
+++ src/components/gif_list.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class GifList extends Component {
+  render() {
+    <Gif id=1 />, <Gif id=2 />
+  }
+}
+
+export default Gif;
